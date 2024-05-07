8dd412dd5b3bd208bddb3d31569045f2c9ea9e59
Wan Muzaffar <wanmuz86@gmail.com>
2024-05-07 11:48:39 +0800

    Listview and Container

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -9,6 +9,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, String>> _todos = [
+    {
+      "name": "Navigation",
+      "desc": "Basic Navigation, Pass forward, Pass back data",
+      "place": "Online meeting"
+    },
+    {
+      "name": "ListView",
+      "desc": "Listview, ListTile, Card",
+      "place": "Online Meeting"
+    },
+    {
+      "name": "Lunch",
+      "desc": "Grab something for lunch",
+      "place": "On place/ home"
+    },
+    {
+      "name": "Shared Preference",
+      "desc": "Local way of storing data",
+      "place": "Online meeting"
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +39,25 @@ class _HomePageState extends State<HomePage> {
         title: Text("To do app"),
         backgroundColor: Colors.red,
       ),
-      body: Text("To do app"),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          // How many rows are there?  -> The length of your data
+          itemCount: _todos.length,
+
+          // What to show on every row
+          itemBuilder: (BuildContext context, int index) {
+            // It will have a container
+            // The height of the container is 50
+            // The color of the container is amber
+            // The container will have a centered child, with text  ...
+            // the value "name" of each item in _todos List (index)
+            return Container(
+              height: 50,
+              color: Colors.amber,
+              child: Center(child: Text(_todos[index]["name"]!)),
+            );
+          }
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
