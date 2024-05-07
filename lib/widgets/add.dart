b71e4df65123f01513833ffbabc2ecab687b8020
Wan Muzaffar <wanmuz86@gmail.com>
2024-05-07 10:58:45 +0800

    Add pages and FAB

---
+++ lib/widgets/add.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Add new item"),),
+      body: Text("Add new item"),
+    );
+  }
+}
